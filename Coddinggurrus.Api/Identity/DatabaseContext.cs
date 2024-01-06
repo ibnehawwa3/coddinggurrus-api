5ecbe12f7c7d944c3349cdbd43afb5a3bc7b8eec
nasirkiyani360 <nasirkiyani360@gmail.com>
2024-01-07 01:28:39 +0500

    core identity configured and db created with identity tables.

---
+++ Coddinggurrus.Api/Identity/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coddinggurrus.Api.Identity
+{
+    public class DatabaseContext : IdentityDbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        // Additional properties, if any
+    }
+}
--- Coddinggurrus.Api/Program.cs
+++ Coddinggurrus.Api/Program.cs
@@ -1,3 +1,7 @@
+using Coddinggurrus.Api.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region Identity
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:CoddingGurrusDb")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+.AddEntityFrameworkStores<DatabaseContext>()
+.AddDefaultTokenProviders();
+#endregion
 
 var app = builder.Build();
 
