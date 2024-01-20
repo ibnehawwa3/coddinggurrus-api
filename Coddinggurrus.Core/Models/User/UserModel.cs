18c23d011f08eae531736fd38583f1b59b5914cc
nasirkiyani360 <nasirkiyani360@gmail.com>
2024-01-20 23:50:41 +0500

    user list endpoint modified.

--- Coddinggurrus.Core/Models/User/UserModel.cs
+++ Coddinggurrus.Core/Models/User/UserModel.cs
@@ -8,6 +8,8 @@ namespace Coddinggurrus.Core.Models.User
 {
     public class UserModel
     {
-
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateRegisteration { get; set; }
     }
 }
