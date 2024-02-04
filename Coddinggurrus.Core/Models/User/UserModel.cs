6a31901b586b38c2858c4d3a106df4dbd9b3f408
Niaz Ali <niaziyaass@gmail.com>
2024-02-04 17:40:58 +0500

    commit changes in model for grid

--- Coddinggurrus.Core/Models/User/UserModel.cs
+++ Coddinggurrus.Core/Models/User/UserModel.cs
@@ -9,6 +9,7 @@ namespace Coddinggurrus.Core.Models.User
     public class UserModel
     {
         public string Email { get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public DateTime DateRegisteration { get; set; }
         public int TotalRecords { get; set; }
