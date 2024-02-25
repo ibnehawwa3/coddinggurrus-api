af1ec8f7db61061a7f220fd82e80c4b9797be43a
nasirkiyani360 <nasirkiyani360@gmail.com>
2024-02-25 23:50:55 +0500

    Content controller add.

---
+++ Coddinggurrus.Api/Controllers/Admin/Tutorials/ContentController.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Coddinggurrus.Api.Controllers.Admin.Tutorials
+{
+    public class ContentController : AdminController
+    {
+        public ContentController(IMapper mapper, IConfiguration config) : base(mapper, config)
+        {
+        }
+    }
+}
