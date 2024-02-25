a8f0f187e89ef802ca9c4a234bfc0e9fb4fc4c91
Niaz Ali <niaziyaass@gmail.com>
2024-02-25 23:46:23 +0500

    commit

--- Coddinggurrus.Core/Entities/RoleMenuPermission.cs
+++ Coddinggurrus.Core/Entities/RoleMenuPermission.cs
@@ -9,7 +9,7 @@ namespace Coddinggurrus.Core.Entities
     public class RoleMenuPermission : Entity<int>
     {
         public long Id { get; set; }
-        public long RoleId { get; set; }
+        public string RoleId { get; set; }
         public string? MenuName { get; set; }
         public long MenuId { get; set; }
         public bool Add { get; set; }
