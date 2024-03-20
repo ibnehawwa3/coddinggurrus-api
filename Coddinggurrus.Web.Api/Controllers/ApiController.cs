64c91ea56341c9f5c1aa7825299bfa064763dd9f
nasirkiyani360 <nasirkiyani360@gmail.com>
2024-03-21 00:30:25 +0500

    New API project add for web apis.

---
+++ Coddinggurrus.Web.Api/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coddinggurrus.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+
+    }
+}
