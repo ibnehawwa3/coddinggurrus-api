928723f17626866e44ec03f6dff80046e35f0623
nasirkiyani53 <nasirkiyani53learning@gmail.com>
2024-03-27 00:00:14 +0500

    Problems faced topic and content add.

---
+++ Coddinggurrus.Api/Controllers/Admin/ProblemsFaced/PFacedContentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coddinggurrus.Api.Controllers.Admin.ProblemsFaced
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PFacedContentController : ControllerBase
+    {
+    }
+}
